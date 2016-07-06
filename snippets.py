43ddf79344d7dd7c361085458b2a869c87af71c3
neil0212 <wonderwall.wong@gmail.com>
2016-07-06 04:41:34 +0000

    Add parser structure

---
+++ snippets.py
@@ -0,0 +1,71 @@
+import logging
+import argparse
+
+# Set the log output file, and the log level
+logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+def put(name, snippet):
+    """
+    Store a snippet with an associated name.
+
+    Returns the name and the snippet
+    """
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    return name, snippet
+    
+def get(name):
+    """Retrieve the snippet with a given name.
+
+    If there is no such snippet, return '404: Snippet Not Found'.
+
+    Returns the snippet.
+    """
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    return ""
+    
+def main():
+    
+
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="Name of the snippet")
+    put_parser.add_argument("snippet", help="Snippet text")
+    
+    # Subparser for the get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Get a snippet")
+    get_parser.add_argument("name", help="Name of the snippet")
+    
+    arguments = parser.parse_args()
+    
+    # Convert parsed arguments from Namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
+        
+        
+    # Code without argument unpacking
+    put(name="list", snippet="A sequence of things - created using []")
+
+    # Identical code which uses argument unpacking
+    arguments = {
+        "name": "list",
+        "snippet": "A sequence of things - created using []"
+                }
+    put(**arguments)
+
+if __name__ == "__main__":
+    
+    main()
